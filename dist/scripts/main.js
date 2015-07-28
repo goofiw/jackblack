1e40453478e5616b16443224e8919c1e63ba610d
goofiw <goofiw@gmail.com>
2015-07-28 12:03:27 -0700

    styled card layout

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,16 +1,37 @@
+var ObjectStack = function() {
+  this.object = {};
+  this.stack = [];
+}
+
+ObjectStack.prototype.push = function(key, value) {
+  this.object[key] = value;
+  this.stack.push(key);
+}
+
+ObjectStack.prototype.pop = function(){
+  var key = this.stack.pop();
+  var prop = this.object[key];
+  delete this.object[key];
+  return [key, prop];
+}
+
 var Board = React.createClass({displayName: "Board",
   getInitialState: function(){
     return {
-        dealerHand: [],
-        playerHand: []
+        dealerHand: new ObjectStack(),
+        playerHand: new ObjectStack()
         }
   },
   render: function(){
     var deck = this.props.deck;
-    this.state.dealerHand.concat(deck.pop());
-    this.state.dealerHand.concat(deck.pop());
-    this.state.playerHand.concat(deck.pop());
-    this.state.playerHand.concat(deck.pop());
+    temp = deck.pop();
+    this.state.dealerHand.push(temp[0],temp[1]);
+    temp = deck.pop();
+    this.state.dealerHand.push(temp[0],temp[1]);
+    temp = deck.pop();
+    this.state.playerHand.push(temp[0],temp[1]);
+    temp = deck.pop();
+    this.state.playerHand.push(temp[0],temp[1]);
     return (
         React.createElement("div", {className: "board"}, 
           React.createElement(Dealer, {hand: this.state.dealerHand}), 
@@ -19,6 +40,15 @@ var Board = React.createClass({displayName: "Board",
         );
   }
 });
+
+var Deck = React.createClass({displayName: "Deck",
+  render: function(){
+    return (
+        React.createElement("div", {className: "card", id: "deck"}
+        )
+        )
+  }
+})
 
 var Dealer = React.createClass({displayName: "Dealer",
   render: function(){
@@ -32,8 +62,11 @@ var Dealer = React.createClass({displayName: "Dealer",
 
 var Player = React.createClass({displayName: "Player",
   render: function(){
+    var currentPlayer = true;
     return (
-        React.createElement("div", {className: "player"}
+        React.createElement("div", {className: "player"}, 
+          React.createElement(Card, {cards: this.props.hand, 
+                currentPlayer: currentPlayer})
         )
         );
   }
@@ -43,12 +76,18 @@ var Card = React.createClass({displayName: "Card",
   render: function() {
     console.log(this.props.cards);
     var allCards = [];
-    for (var key in this.props.deck) {
-      if(this.props.deck.hasOwnProperty(key)){
-        var suit, rank;
+    for (var i = 0;  i < this.props.cards.stack.length; i++) {
+        var suit, rank, stack, obj, key;
+        stack = this.props.cards.stack;
+        obj = this.props.cards.object;
+        key = stack[i];
         suit = key[0];
         rank = key.slice(1);
         suitClass = 'card suit';
+        if (i === 0 && !this.props.currentPlayer) {
+          rank='';
+          suit = '';
+        }
         allCards.push(
             React.createElement("div", {className: suitClass}, 
               React.createElement("span", {className: "value"}, rank), 
@@ -57,9 +96,8 @@ var Card = React.createClass({displayName: "Card",
             )
             );
       }
-    }
     return(
-      React.createElement("div", null, 
+      React.createElement("div", {className: "hand"}, 
         allCards
       )
     )
@@ -69,11 +107,11 @@ var Card = React.createClass({displayName: "Card",
 var Game = React.createClass({displayName: "Game",
   getInitialState: function(){
     return {
-    deck: this.createDeck()
+    deck: this.shuffleDeck(this.createDeck())
     };
   },
   createDeck: function(){
-    var suitSet = {}, newDeck;
+    var newDeck = new ObjectStack();
     var suits = ['s', 'd', 'h', 'c'];
     //reduce was skipping over spades for some reason
     //Creats an object that holds the rendering info in the key
@@ -81,19 +119,29 @@ var Game = React.createClass({displayName: "Game",
     suits.forEach(function(suit) {
       for(var i = 1; i < 14; i ++){
         if( i == 11) {
-          suitSet[suit.concat('J')] = i;  
+          newDeck.push(suit.concat('J'), 10);  
         } else if (i === 12){
-          suitSet[suit.concat('Q')] = i;
+          newDeck.push(suit.concat('Q'), 10);
         } else if (i === 13){
-          suitSet[suit.concat('K')] = i;
+          newDeck.push(suit.concat('K'), 10);
         } else if (i === 1) {
-          suitSet[suit.concat('A')] = i;
+          newDeck.push(suit.concat('A'), 11);
         } else {
-          suitSet[suit.concat(i)] = i;  
+          newDeck.push(suit.concat(i), i);  
         }
       }
-    });
-    return suitSet;
+    })
+    return newDeck;
+  },
+  shuffleDeck: function(deck){
+    var arr = deck.stack;
+    for (var i = arr.length - 1; i > 0;  i--){
+      var swap = Math.floor(Math.random() * (i + 1));
+      var temp = arr[i];
+      arr[i] = arr[swap];
+      arr[swap] = temp; 
+    }
+    return deck;
   },
   render: function(){
     return (
--- src/scripts/application.jsx
+++ src/scripts/application.jsx
@@ -1,16 +1,37 @@
+var ObjectStack = function() {
+  this.object = {};
+  this.stack = [];
+}
+
+ObjectStack.prototype.push = function(key, value) {
+  this.object[key] = value;
+  this.stack.push(key);
+}
+
+ObjectStack.prototype.pop = function(){
+  var key = this.stack.pop();
+  var prop = this.object[key];
+  delete this.object[key];
+  return [key, prop];
+}
+
 var Board = React.createClass({
   getInitialState: function(){
     return {
-        dealerHand: [],
-        playerHand: []
+        dealerHand: new ObjectStack(),
+        playerHand: new ObjectStack()
         }
   },
   render: function(){
     var deck = this.props.deck;
-    this.state.dealerHand.concat(deck.pop());
-    this.state.dealerHand.concat(deck.pop());
-    this.state.playerHand.concat(deck.pop());
-    this.state.playerHand.concat(deck.pop());
+    temp = deck.pop();
+    this.state.dealerHand.push(temp[0],temp[1]);
+    temp = deck.pop();
+    this.state.dealerHand.push(temp[0],temp[1]);
+    temp = deck.pop();
+    this.state.playerHand.push(temp[0],temp[1]);
+    temp = deck.pop();
+    this.state.playerHand.push(temp[0],temp[1]);
     return (
         <div className="board">
           <Dealer hand={this.state.dealerHand}/>
@@ -19,6 +40,15 @@ var Board = React.createClass({
         );
   }
 });
+
+var Deck = React.createClass({
+  render: function(){
+    return (
+        <div className="card" id="deck">
+        </div>
+        )
+  }
+})
 
 var Dealer = React.createClass({
   render: function(){
@@ -32,8 +62,11 @@ var Dealer = React.createClass({
 
 var Player = React.createClass({
   render: function(){
+    var currentPlayer = true;
     return (
         <div className="player">
+          <Card cards={this.props.hand}
+                currentPlayer={currentPlayer}/>
         </div>
         );
   }
@@ -43,12 +76,18 @@ var Card = React.createClass({
   render: function() {
     console.log(this.props.cards);
     var allCards = [];
-    for (var key in this.props.deck) {
-      if(this.props.deck.hasOwnProperty(key)){
-        var suit, rank;
+    for (var i = 0;  i < this.props.cards.stack.length; i++) {
+        var suit, rank, stack, obj, key;
+        stack = this.props.cards.stack;
+        obj = this.props.cards.object;
+        key = stack[i];
         suit = key[0];
         rank = key.slice(1);
         suitClass = 'card suit';
+        if (i === 0 && !this.props.currentPlayer) {
+          rank='';
+          suit = '';
+        }
         allCards.push(
             <div className={suitClass}>
               <span className='value'>{rank}</span>
@@ -57,9 +96,8 @@ var Card = React.createClass({
             </div>
             );
       }
-    }
     return(
-      <div>
+      <div className='hand'>
         {allCards}
       </div>
     )
@@ -69,11 +107,11 @@ var Card = React.createClass({
 var Game = React.createClass({
   getInitialState: function(){
     return {
-    deck: this.createDeck()
+    deck: this.shuffleDeck(this.createDeck())
     };
   },
   createDeck: function(){
-    var suitSet = {}, newDeck;
+    var newDeck = new ObjectStack();
     var suits = ['s', 'd', 'h', 'c'];
     //reduce was skipping over spades for some reason
     //Creats an object that holds the rendering info in the key
@@ -81,19 +119,29 @@ var Game = React.createClass({
     suits.forEach(function(suit) {
       for(var i = 1; i < 14; i ++){
         if( i == 11) {
-          suitSet[suit.concat('J')] = 10;  
+          newDeck.push(suit.concat('J'), 10);  
         } else if (i === 12){
-          suitSet[suit.concat('Q')] = 10;
+          newDeck.push(suit.concat('Q'), 10);
         } else if (i === 13){
-          suitSet[suit.concat('K')] = 10;
+          newDeck.push(suit.concat('K'), 10);
         } else if (i === 1) {
-          suitSet[suit.concat('A')] = 11;
+          newDeck.push(suit.concat('A'), 11);
         } else {
-          suitSet[suit.concat(i)] = i;  
+          newDeck.push(suit.concat(i), i);  
         }
       }
-    });
-    return suitSet;
+    })
+    return newDeck;
+  },
+  shuffleDeck: function(deck){
+    var arr = deck.stack;
+    for (var i = arr.length - 1; i > 0;  i--){
+      var swap = Math.floor(Math.random() * (i + 1));
+      var temp = arr[i];
+      arr[i] = arr[swap];
+      arr[swap] = temp; 
+    }
+    return deck;
   },
   render: function(){
     return (
